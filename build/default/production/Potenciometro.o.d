c3522aeec787127012eee271753ac8fc45ce7493
Zanolin2 <matiasfernandezzanolin@gmail.com>
2026-03-12 17:44:03 -0300

    Add files via upload

---
+++ build/default/production/Potenciometro.o.d
@@ -0,0 +1,8 @@
+build/default/production/Potenciometro.o: Potenciometro.c Potenciometro.h \
+ mcc_generated_files/spi2.h mcc_generated_files/pin_manager.h
+
+Potenciometro.h:
+
+mcc_generated_files/spi2.h:
+
+mcc_generated_files/pin_manager.h:
---
+++ build/default/production/main.o.d
@@ -0,0 +1,15 @@
+build/default/production/main.o: main.c mcc_generated_files/system.h \
+ mcc_generated_files/system_types.h Potenciometro.h Excitacion.h ADC.h \
+ LCD_I2C.h
+
+mcc_generated_files/system.h:
+
+mcc_generated_files/system_types.h:
+
+Potenciometro.h:
+
+Excitacion.h:
+
+ADC.h:
+
+LCD_I2C.h:
